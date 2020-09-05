1c3e70b20bec3923892288e30fa4b4c20d0fbee0
yuya.nakagawa <yuye4049@gmail.com>
2020-09-05 05:09:45 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #usersを扱うルーティング
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create, :edit, :update, :destroy]
 end
