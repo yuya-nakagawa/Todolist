8ca1864b80c516ae6e807a4f3890fed603addc11
yuya.nakagawa <yuye4049@gmail.com>
2020-09-08 02:05:32 +0000

    add to edit_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスク名を変更しました'
+      redirect_to edit_task_path
+    else
+      flash.now[:danger] = 'タスク名の変更が失敗しました'
+      render :edit
+    end
   end
 
   def destroy
